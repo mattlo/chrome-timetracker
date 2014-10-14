9ec968073a5d6671bcef13fcf402ab4b74e053ee
Matt Lo <mattlo.developer@gmail.com>
2014-10-14 16:42:14 -0500

    refactored directory structure, added SCSS structure and watcher util script

--- src/js/background-runner.js
+++ src/js/background-runner.js
@@ -1,5 +1,5 @@
 chrome.app.runtime.onLaunched.addListener(function () {
-	chrome.app.window.create('app.html', {
+	chrome.app.window.create('src/views/app.html', {
 		id: "chromeTimetracker",
 		innerBounds: {
 			width: 244,
---
+++ util/scss-watcher.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mkdir -p src/css
+scss --watch src/scss/main.scss:src/css/main.css
