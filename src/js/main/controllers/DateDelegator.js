7c99845b44c3c0f854307ffe9361816faa5a30b3
Matt Lo <mattlo.developer@gmail.com>
2015-01-21 21:51:36 -0600

    added week traversing

--- src/js/main/controllers/DateDelegator.js
+++ src/js/main/controllers/DateDelegator.js
@@ -1,5 +1,5 @@
 // @TODO current digest cycle is running 3 times, and it has nothing to do with any of the impl
-app.controller('DateDelegator', function ($scope, WeekFactory, Util) {
+app.controller('DateDelegator', function ($rootScope, $scope, $log, WeekFactory, Util) {
 
 	// constants
 	// @TODO should we use angular constants?
@@ -25,23 +25,65 @@ app.controller('DateDelegator', function ($scope, WeekFactory, Util) {
 	$scope.tasks = {};
 
 	/**
+	 * @type {Moment}
+	 */
+	$scope.weekContext = moment();
+
+	/**
 	 * Initializer
 	 * @return {undefined} 
 	 */
 	$scope.init = function () {
-		$scope.setWeek();
+		$scope.setWeek($scope.weekContext);
+
+		function setWeekContext(method, n) {
+			var d = $scope.weekContext;
+
+			$scope.weekContext = d[method].call(d, n, 'days')
+
+			$scope.setWeek(d);
+		}
+
+		// event set current week
+		$rootScope.$on('setCurrentWeek', function () {
+			$log.info('setting to this week...');
+
+			$scope.weekContext = moment();
+
+			$scope.setWeek($scope.weekContext);
+		});
+
+		// event set last week
+		$rootScope.$on('setLastWeek', function () {
+			$log.info('setting to last week');
+
+			setWeekContext('subtract', 7);
+		});
+
+		// event set next week
+		$rootScope.$on('setNextWeek', function () {
+			$log.info('setting to next week');
+
+			setWeekContext('add', 7);
+		});
 	};
 
 	/**
 	 * Create week configuration
 	 * @param {String|Date|Moment} date 
+	 * @param {Boolean} clearWeeks Defaults to true, clears weeks before appending
 	 * @return {undefined}
 	 */
-	$scope.setWeek = function (date) {
+	$scope.setWeek = function (date, clearWeeks) {
 		var days = $scope.createWeek(date),
 			start = days[$scope.START_DAY],
 			end = days[$scope.END_DAY],
 			tasks = $scope.getTasks(start, end);
+
+		// clear weeks
+		if (clearWeeks !== false) {
+			$scope.weeks = [];
+		}
 
 		// push out week info
 		$scope.weeks.push({
